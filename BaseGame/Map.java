1eb89e5f30f77e1b805cc50f87d6d40a5146bf3e
rfw118 <36180359+rfw118@users.noreply.github.com>
2018-04-29 21:49:01 -0400

    Added Map class and command
    Added a 4x4 map to the "MyGame" object which can be made into whatever size we deem necessary.  Added the command to display the map on the interface.

---
+++ BaseGame/Map.java
@@ -0,0 +1,48 @@
+package BaseGame;
+
+import com.sun.javafx.geom.Point2D;
+
+public class Map {
+	
+	public Map(int x, int y)
+	{
+		GameMap = new String[x][y];
+		ptLocation.setLocation(0, 0); //Starting point on the map
+		for(int i = 0; i < GameMap.length; i++)
+		{
+			for(int j = 0; j < GameMap.length; j++)
+			{
+				GameMap[i][j] = ".";
+			}
+		}
+		
+		GameMap[(int) ptLocation.x][(int) ptLocation.y] = "@";
+		
+	}
+
+	String[][] GameMap;
+	Point2D ptLocation = new Point2D();
+	
+	public String getMap()
+	{
+		String sMap = "";
+		for(int i = 0; i < GameMap.length; i++)
+		{
+			for(int j = 0; j < GameMap.length; j++)
+			{
+				sMap += GameMap[i][j].toString();
+			}
+			sMap += "\r\n"; //add a new line to the string.
+		}
+				
+		return sMap;
+	}
+	//bob
+	public Point2D getLocation()
+	{
+		return ptLocation;
+	}
+	
+	
+
+}
