48abded44a296b09c8b7f910cf7c6b649f379157
JeremyDavison <36427504+JeremyDavison@users.noreply.github.com>
2018-05-13 21:23:00 -0400

    Add files via upload

---
+++ StateMachine/StatsCommand.java
@@ -0,0 +1,33 @@
+package BaseGame;
+
+import UI.Commandable;
+import UI.MyGame;
+import UI.UserInterface;
+
+public class StatsCommand implements Commandable {
+
+	public StatsCommand(MyGame g)
+	{
+		g.Subscribe(this);
+	}
+	
+	@Override
+	public void doCommand(MyGame g) {
+		UserInterface.UserOutput("HP: " + g.player.getTotalHealth() + " / " + g.player.getHealth(), true);
+		UserInterface.UserOutput("Time Remaining to Evacuatoin: " + g.player.getTime() + " parsecs", true);
+		g.HelpMe.doCommand(g);
+	}
+
+	@Override
+	public boolean matchCommand(String s) {
+		return s.toUpperCase().equals(getCommandText().toUpperCase()); //returns true for match and false for no match.
+		
+	}
+
+	@Override
+	public String getCommandText() {
+		// TODO Auto-generated method stub
+		return "stats";
+	}
+
+}
